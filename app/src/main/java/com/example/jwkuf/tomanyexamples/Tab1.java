b5d9d21ddcb3a37b0a376c756d4010774e635e71
Jake <jwkufner@gmail.com>
2018-03-14 22:32:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jwkuf/tomanyexamples/Tab1.java
@@ -0,0 +1,20 @@
+package com.example.jwkuf.tomanyexamples;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab1 extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab1, container, false);
+    }
+}
